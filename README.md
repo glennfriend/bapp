7023327fd8958dc058bc89528c5816774ecae4bb
glenn guan <glenn.profile@gmail.com>
2023-02-17 09:00:29 +0800

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# bash app install
+- install your app/software
+
+## install
+```bash
+source <( curl -sS --insecure https://raw.githubusercontent.com/glennfriend/bapp/master/setup.sh )
+```
---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+bash_app_install() {
+
+    if [ "$1" = 'is-dev' ] ; then
+        APP_REPOSITORY="git@github.com:glennfriend/bapp.git"
+    elif  [ "$1" = 'is-end-user' ] ; then
+        APP_REPOSITORY="https://github.com/glennfriend/bapp.git"
+    else
+        return
+    fi
+
+    APP_PATH=~/tools/bapp
+    APP_RESOURCE=~/.bapp
+    mkdir -p                    ${APP_PATH}
+    mkdir -p                    ${APP_RESOURCE}
+    git clone ${APP_REPOSITORY} ${APP_PATH}
+    chmod +x                    ${APP_PATH}/src/bapp.sh
+    ln -sfn                     ${APP_PATH}/src/bapp.sh  /bin/bapp
+
+}
+
+bash_app_install 'is-end-user'
