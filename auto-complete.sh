6106abe4a25e6a8dc072ea48eda6c898d7a74ed0
glenn guan <glenn.profile@gmail.com>
2023-02-17 14:52:00 +0800

    basic update

--- auto-complete.sh
+++ auto-complete.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
 TOOL_PATH="$(dirname "$(readlink -f $0)")"
-SCRIPT_FILE_LIST=$(ls -R "${TOOL_PATH}/src/" | grep  '^[^\/]' )
+#SCRIPT_FILE_LIST=$(ls -R "${TOOL_PATH}/src/" | grep  '^[^\/]' )
 
+cd ${TOOL_PATH}/src
+SCRIPT_FILE_LIST=$(find ./ -type f | sed -e "s/\.\///g" )
+cd -    2>/dev/null
+
+#find ${TOOL_PATH}/src  -type f | sed -e "s/${TOOL_PATH}//g" 
 
 # complete -W "aaa bbb ccc" bapp
 complete -W "${SCRIPT_FILE_LIST}" bapp
